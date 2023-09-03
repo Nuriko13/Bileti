d9d2445c051b46297ccc90922e2640689596f992
Neto test <nurlan264@mail.ru>
2023-09-03 11:51:09 +0300

    bileti

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 13_676;
+
+        int bonus = price / 28;
+        System.out.println("бонус:" + bonus);
+
+    }
+
+
+}
